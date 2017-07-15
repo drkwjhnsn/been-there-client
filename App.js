0b6a1d5a34210a762bbfbbab652e944c693b4413
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-07-15 10:42:58 -0700

    Implement client scaffolding

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Settings from './src/settings/Settings.js';
+import ModalBar from './src/ModalBar.js';
+import CreatePosts from './src/CreatePosts.js';
+import PostList from './src/PostList.js';
+import PostPreview from './src/PostPreview.js';
+import PostView from './src/PostView.js';
+import SignIn from './src/SignIn.js';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>This is Nick's phone!!</Text>
+        <Settings />
+        <ModalBar />
+        <CreatePosts />
+        <PostList />
+        <PostPreview />
+        <PostView />
+        <SignIn />
       </View>
     );
   }
